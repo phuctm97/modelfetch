3f9949e3e2539920d5ccf2e1973f84a367543a5b
phuctm97 <phuctm97@gmail.com>
2025-06-29 12:14:53 +0700

    Initialize projects

---
+++ libs/nx-10x/src/generators/format-tsconfigs/index.ts
@@ -0,0 +1,43 @@
+import type { Tree } from "@nx/devkit";
+import type { SyncGeneratorResult } from "nx/src/utils/sync-generators";
+import type { TsConfigJson } from "type-fest";
+
+import {
+  createProjectGraphAsync,
+  formatFiles,
+  readProjectsConfigurationFromProjectGraph,
+  updateJson,
+} from "@nx/devkit";
+
+function formatConfigs(tree: Tree, root?: string) {
+  for (const ext of ["json", "lib.json", "app.json", "build.json"]) {
+    const tsConfigPath = root ? `${root}/tsconfig.${ext}` : `tsconfig.${ext}`;
+    if (!tree.exists(tsConfigPath)) continue;
+    updateJson<TsConfigJson, TsConfigJson>(tree, tsConfigPath, (json) => {
+      json.references?.sort((a, b) => a.path.localeCompare(b.path, "en"));
+      if (
+        (json.include && json.include.length > 0) ||
+        (json.files && json.files.length > 0)
+      ) {
+        json.compilerOptions ??= {};
+        json.compilerOptions.sourceRoot = `/${root ?? ""}`;
+        if (json.compilerOptions.rootDir)
+          json.compilerOptions.sourceRoot += `/${json.compilerOptions.rootDir}`;
+      }
+      return json;
+    });
+  }
+}
+
+export default async function formatTsConfigs(
+  tree: Tree,
+): Promise<SyncGeneratorResult> {
+  const { projects } = readProjectsConfigurationFromProjectGraph(
+    await createProjectGraphAsync(),
+  );
+  for (const project of Object.values(projects))
+    formatConfigs(tree, project.root);
+  formatConfigs(tree);
+  await formatFiles(tree);
+  return { outOfSyncMessage: "Some tsconfig.json files are unformatted." };
+}
---
+++ libs/nx-10x/src/index.ts
@@ -0,0 +1,67 @@
+import type {
+  CreateNodesV2,
+  ProjectType,
+  TargetConfiguration,
+} from "@nx/devkit";
+import type { PackageJson } from "type-fest";
+
+import { createNodesFromFiles, readJsonFile } from "@nx/devkit";
+import fs from "node:fs";
+import path from "node:path";
+
+function getProjectType(projectRoot: string): ProjectType | undefined {
+  if (projectRoot.startsWith("apps")) return "application";
+  if (projectRoot.startsWith("libs")) return "library";
+}
+
+export const createNodesV2: CreateNodesV2 = [
+  "**/package.json",
+  (packageJsonFilePaths, options, context) =>
+    createNodesFromFiles(
+      (packageJsonFilePath) => {
+        const packageJson = readJsonFile<PackageJson>(packageJsonFilePath);
+        if (!packageJson.name) return {};
+        const projectRoot = path.dirname(packageJsonFilePath);
+        const projectType = getProjectType(projectRoot);
+        const targets: Record<string, TargetConfiguration> = {};
+        if (fs.existsSync(path.join(projectRoot, "eslint.config.js"))) {
+          targets.lint = {
+            cache: true,
+            command: "eslint --no-error-on-unmatched-pattern",
+            options: { cwd: "{projectRoot}" },
+            inputs: [
+              "default",
+              "^default",
+              { externalDependencies: ["typescript", "eslint"] },
+            ],
+            syncGenerators: ["@nx/js:typescript-sync"],
+          };
+        }
+        if (packageJson.devDependencies?.["@modelfetch/cli"]) {
+          targets.build = {
+            cache: true,
+            command: "modelfetch build",
+            options: { cwd: "{projectRoot}" },
+            inputs: [
+              "production",
+              "^production",
+              { externalDependencies: ["typescript", "tslib"] },
+            ],
+            outputs: ["{projectRoot}/dist"],
+            dependsOn: ["^build"],
+            syncGenerators: ["@nx/js:typescript-sync"],
+          };
+          targets.start = {
+            command: "modelfetch start",
+            options: { cwd: "{projectRoot}" },
+            continuous: true,
+            dependsOn: ["build"],
+          };
+        }
+        return { projects: { [projectRoot]: { projectType, targets } } };
+      },
+      packageJsonFilePaths,
+      options,
+      context,
+    ),
+];
