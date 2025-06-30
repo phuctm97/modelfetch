2943c9dc8dcefdc0fcc6d1124cb508b856814e4c
phuctm97 <phuctm97@gmail.com>
2025-06-30 10:28:54 +0700

    Add @modelfetch/cli as a devDependency in JavaScript and TypeScript examples, update tsconfig references, and enhance modelfetch-cli with CLI commands and dependencies.

--- libs/modelfetch-cli/src/index.ts
+++ libs/modelfetch-cli/src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello, world!");
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+  .name("modelfetch")
+  .description("CLI to build ModelFetch applications")
+  .version("0.0.1");
+
+program
+  .command("build")
+  .description("Build a ModelFetch application")
+  .action(() => {
+    console.error("NOT_IMPLEMENTED");
+  });
+
+program.parse();
