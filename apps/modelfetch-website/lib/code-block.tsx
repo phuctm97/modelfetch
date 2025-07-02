a87e6518d6e2897ceef707b2665c4cb363c33b40
phuctm97 <phuctm97@gmail.com>
2025-07-02 16:28:16 +0700

    Refactor CodeBlock component to use Fumadocs UI
    - Updated the CodeBlock component to utilize FumadocsCodeBlock and FumadocsPre for rendering, enhancing consistency with the Fumadocs library.
    - Adjusted the CodeBlockProps interface to extend from FumadocsCodeBlockProps, excluding the children prop for better type safety.
    
    These changes improve the integration with Fumadocs and streamline the code structure.

--- apps/modelfetch-website/lib/code-block.tsx
+++ apps/modelfetch-website/lib/code-block.tsx
@@ -1,18 +1,24 @@
 import type { HighlightOptionsCommon } from "fumadocs-core/highlight";
+import type { CodeBlockProps as FumadocsCodeBlockProps } from "fumadocs-ui/components/codeblock";
+import type { Except } from "type-fest";
 
 import { highlight } from "fumadocs-core/highlight";
-import * as Base from "fumadocs-ui/components/codeblock";
+import {
+  CodeBlock as FumadocsCodeBlock,
+  Pre as FumadocsPre,
+} from "fumadocs-ui/components/codeblock";
 
 const components: HighlightOptionsCommon["components"] = {
-  pre: (props) => <Base.Pre {...props} />,
+  pre: (props) => <FumadocsPre {...props} />,
 };
 
-export interface CodeBlockProps extends Base.CodeBlockProps {
+export interface CodeBlockProps
+  extends Except<FumadocsCodeBlockProps, "children"> {
   code: string;
   lang: HighlightOptionsCommon["lang"];
 }
 
 export async function CodeBlock({ code, lang, ...props }: CodeBlockProps) {
-  const rendered = await highlight(code, { lang, components });
-  return <Base.CodeBlock {...props}>{rendered}</Base.CodeBlock>;
+  const children = await highlight(code, { lang, components });
+  return <FumadocsCodeBlock {...props}>{children}</FumadocsCodeBlock>;
 }
