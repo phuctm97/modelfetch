93270dec031f36ebc337a62529f03fbd364a6e94
phuctm97 <phuctm97@gmail.com>
2025-07-04 16:32:07 +0700

    docs: update Cloudflare runtime documentation and examples for clarity and consistency

--- libs/modelfetch-cloudflare/src/index.ts
+++ libs/modelfetch-cloudflare/src/index.ts
@@ -1,22 +1,8 @@
 import type { Server } from "@modelfetch/core";
-import type { Except } from "type-fest";
 
 import { createApp } from "@modelfetch/core";
 
-export type Options<
-  Env = unknown,
-  QueueHandlerMessage = unknown,
-  CfHostMetadata = unknown,
-> = Except<ExportedHandler<Env, QueueHandlerMessage, CfHostMetadata>, "fetch">;
-
-export default function handle<
-  Env = unknown,
-  QueueHandlerMessage = unknown,
-  CfHostMetadata = unknown,
->(
-  server: Server,
-  options?: Options<Env, QueueHandlerMessage, CfHostMetadata>,
-): ExportedHandler<Env, QueueHandlerMessage, CfHostMetadata> {
+export default function handle(server: Server) {
   const app = createApp(server);
-  return { ...options, fetch: app.fetch };
+  return app.fetch;
 }
