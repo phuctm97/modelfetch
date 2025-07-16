49a2db3c33fcdc268ffe33db26c0afb58b0d0a11
phuctm97 <phuctm97@gmail.com>
2025-07-16 21:26:52 +0700

    Fix

--- apps/example-vercel-ts/eslint.config.js
+++ apps/example-vercel-ts/eslint.config.js
@@ -1,13 +1,12 @@
 import createESLintConfig from "create-eslint-config";
 import importPaths from "eslint-config-import-paths";
 import react from "eslint-config-react";
-import pkg from "@next/eslint-plugin-next";
-const { flatConfig } = pkg;
+import next from "@next/eslint-plugin-next";
 
 const eslintConfig = createESLintConfig(
   importPaths,
   react,
-  flatConfig.coreWebVitals,
+  next.flatConfig.coreWebVitals,
 );
 
 export default eslintConfig;
