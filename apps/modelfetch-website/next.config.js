dfa5544ffe875fb1da937aa4b8ad3f7ebc08d61f
phuctm97 <phuctm97@gmail.com>
2025-07-20 14:28:25 +0700

    Fix

--- apps/modelfetch-website/next.config.js
+++ apps/modelfetch-website/next.config.js
@@ -1,15 +1,20 @@
 import { createMDX } from "fumadocs-mdx/next";
 
-export default createMDX()({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  webpack: process.env.GITHUB_ACTIONS
-    ? (config) => {
-        if (config.cache) config.cache = Object.freeze({ type: "memory" });
-        return config;
-      }
-    : undefined,
-  experimental: process.env.GITHUB_ACTIONS
-    ? { webpackMemoryOptimizations: true, webpackBuildWorker: true }
-    : undefined,
-});
+};
+
+if (process.env.GITHUB_ACTIONS === "true") {
+  nextConfig.webpack = (config) => {
+    if (config.cache) config.cache = Object.freeze({ type: "memory" });
+    return config;
+  };
+  nextConfig.experimental = {
+    webpackMemoryOptimizations: true,
+    webpackBuildWorker: true,
+  };
+}
+
+export default createMDX()(nextConfig);
